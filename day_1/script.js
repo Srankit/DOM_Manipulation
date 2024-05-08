55a598328db8b61c35e0b999f4894022107b3743
Srankit <ankitsarar142@gmail.com>
2024-05-08 15:19:07 +0530

    Day one complete and Day 2 starat

--- day_1/script.js
+++ day_1/script.js
@@ -3,9 +3,28 @@
 // let ele = document.getElementsByClassName('ele');
 // console.log(ele);
 
-let eles= document.getElementsByTagName('p');
-let chng = eles[1].innerText= "i am ankit"
+let eles = document.getElementsByTagName('p');
+let chng = eles[1].innerText = "i am an kit"
 
 console.log(eles)
-let para= document .getElementById('para');
-para.style.backgroundColor="skyblue"
+let para = document.getElementById('para');
+para.style.backgroundColor = "skyblue"
+
+let box = document.getElementById('box').innerText;
+console.log(box);
+
+
+// box1.style.paddingLeft="10px",box1.style.paddingRight="10px";
+// console.log(box1);
+// let box1= document.getElementById('box');
+// box1.innerHTML = "<h1>new Hello</h1>"
+// let styl= document.getElementById('box');
+
+// event handling
+
+function logSomthing(){
+    let box1 = document.getElementById('box');
+box1.innerText= "This command will commit your changes with the message All code Add. If you want to include more descriptive information in your commit message, you can use a longer message by omitting the -m option, which will open your default text editor for you to write the commit message. For example";
+}
+document.getElementById('box')
+.addEventListener('mouseenter', logSomthing);
