5e06716ffdb1d140afa7292339b53945a428f16e
Srankit <ankitsarar142@gmail.com>
2024-05-08 13:06:53 +0530

    All code Add

---
+++ day_1/script.js
@@ -0,0 +1,11 @@
+// document.getElementById('para').innerText = "my name is ankit";
+
+// let ele = document.getElementsByClassName('ele');
+// console.log(ele);
+
+let eles= document.getElementsByTagName('p');
+let chng = eles[1].innerText= "i am ankit"
+
+console.log(eles)
+let para= document .getElementById('para');
+para.style.backgroundColor="skyblue"
