a8bf10358b25fca06ae0a2706fb7973707994564
Srankit <ankitsarar142@gmail.com>
2024-05-09 17:54:31 +0530

    Day_3 complete

---
+++ day_3/index.js
@@ -0,0 +1,9 @@
+console.log("day _3 started");
+let li_list = document.getElementsByTagName("li");
+console.log(li_list);
+let ni = document.createElement("li");
+ni.innerText = "Hawkeye";
+document.getElementById("list-item").appendChild(ni);
+console.log(li_list);
+let item = document.querySelectorAll("li");
+console.log(item);
